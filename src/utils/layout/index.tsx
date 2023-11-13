ef53abe0ed1be9684ab0fb9c25db9f7a8ec1dd26
MaistrukHUB <vladyslavmaistruk07@gmail.com>
2023-11-13 11:04:17 +0200

    init 2

--- src/utils/layout/index.tsx
+++ src/utils/layout/index.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
+    <div className="container">
       <Header />
       {children}
       <Footer />
