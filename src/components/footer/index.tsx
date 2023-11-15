0160759c2fc07d2e9f638efe4fd7ac2f985aa646
MaistrukHUB <vladyslavmaistruk07@gmail.com>
2023-11-15 13:51:56 +0200

    added footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import style from "./Footer.module.scss";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <footer className={style.root}>Footer</footer>;
+  return (
+    <footer className={style.root}>
+      <Link to={"/"} className={style.logo}>
+        CARMA
+      </Link>
+      <a
+        href='tel:+380960167568'
+        rel='nofollow'
+        className={style.phone}
+      >
+        +380960167568
+      </a>
+      <p className={style.est}>est. 2024</p>
+    </footer>
+  );
 };
 
 export default Footer;
